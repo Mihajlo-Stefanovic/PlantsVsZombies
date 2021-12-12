0e0f1ae562d4e2c0dc5c6feb3ddb758d785b84f6
Orescanin <markoorescaninsd@gmail.com>
2021-12-12 22:27:15 +0100

    implemntirao pucanje i raycasting za pucanje

---
+++ PVSZ_Project/Assets/Scripts/ShotMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMovement : MonoBehaviour
+{
+
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * Time.deltaTime * 10);
+        
+        timer += Time.deltaTime;
+        if (timer > 2){
+            Destroy(this.gameObject);
+        }
+    }
+}
--- PVSZ_Project/Assets/Scripts/TechUnit.cs
+++ PVSZ_Project/Assets/Scripts/TechUnit.cs
@@ -4,4 +4,42 @@ using UnityEngine;
 
 public class TechUnit : FieldUnit
 {
+
+    float distanceToDetect;
+    float attackSpeed = 0.5f;
+    float nextAttack = 0.5f;
+    [SerializeField] private Transform reyPos;
+    [SerializeField] private GameObject shotPrefab;
+    
+    
+
+private void Start() {
+    distanceToDetect = 8;
+    
+}
+
+
+void Update() {
+    
+    Debug.DrawRay(reyPos.position, reyPos.right * distanceToDetect, Color.red);
+
+    Ray ray = new Ray(reyPos.position, reyPos.right);
+
+    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, distanceToDetect, LayerMask.GetMask("Alien"));
+
+    if(hit) {
+        
+        if(Time.time>nextAttack){
+
+            nextAttack = Time.time+attackSpeed;
+            Instantiate(shotPrefab,reyPos.position,Quaternion.identity);
+
+        }
+        
+
+        
+        }
+    
+}
+
 }
