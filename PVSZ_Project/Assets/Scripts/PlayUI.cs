0b40246453e237837c9a1679f3867b7631ddd97b
Blanko <mihajlostefanovic51@gmail.com>
2021-12-21 02:06:18 +0100

    Fix Tech Cards being available on Alien turn

--- PVSZ_Project/Assets/Scripts/PlayUI.cs
+++ PVSZ_Project/Assets/Scripts/PlayUI.cs
@@ -3,25 +3,25 @@ using UnityEngine;
 public class PlayUI : MonoBehaviour
 {
     public EndTurn      endTurn;
-    public TechCard     card;           // NOTE(sftl): handle multiple
+    public GameObject   techCards;
     public RemoveCard   removeCard;
     public GameObject   powerCards;
     
     public void OnAlienTurn()
     {
         endTurn.Dissable();
-        card.Dissable();
         removeCard.Dissable();
         
         powerCards.SetActive(true);
+        techCards.SetActive(false);
     }
     
     public void OnTechTurn()
     {
         endTurn.Enable();
-        card.Enable();
         removeCard.Enable();
         
         powerCards.SetActive(false);
+        techCards.SetActive(true);
     }
 }
