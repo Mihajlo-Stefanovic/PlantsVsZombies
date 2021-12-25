fdcb7994be3e0cfbb2df661b4834fb9468e3d047
Blanko <mihajlostefanovic51@gmail.com>
2021-12-25 17:09:23 +0100

    Add Package "Starter Particle Pack"; Add some particles to the MainScene

--- PVSZ_Project/Assets/Scripts/TechCard.cs
+++ PVSZ_Project/Assets/Scripts/TechCard.cs
@@ -13,20 +13,19 @@ public enum CardType
 public class TechCard : MonoBehaviour, IPointerDownHandler
 {
     public CardType type;
-
+    public ParticleSystem ps;
+    
     public void OnPointerDown(PointerEventData pointerEventData)
     {
-
         GameManager.Instance.TechCardClicked(this);
-
-
+        ps.Play();
     }
-
+    
     public void Enable()
     {
         gameObject.SetActive(true);
     }
-
+    
     public void Dissable()
     {
         gameObject.SetActive(false);
