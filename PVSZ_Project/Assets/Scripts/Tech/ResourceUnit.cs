ff6b882d971f6a43d706d739b8def850b332cc05
blanko <mihajlostefanovic51@gmail.com>
2021-12-30 03:34:21 +0100

    Add gain particles to Resource Unit

--- PVSZ_Project/Assets/Scripts/Tech/ResourceUnit.cs
+++ PVSZ_Project/Assets/Scripts/Tech/ResourceUnit.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class ResourceUnit : TechUnit
 {
     [SerializeField] private int _resourcesToGet;
+    [SerializeField] private ParticleSystem _gainParticles;
+    
     private int currResources;
-
+    
     public void IncreaseRescources()
     {
         currResources = ResourceManager.getResources();
         currResources += _resourcesToGet;
         // Debug.Log("ovoliko resursa" + currResources);
         GameManager.Instance.resourceManager.setResources(currResources);
+        
+        _gainParticles.Play();
     }
 }
